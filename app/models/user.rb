36479a27caad8b997c7f601185f3a994ee6923e7
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-21 18:37:00 +0200

    add username field for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
          :validatable # validate email and password
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 end
