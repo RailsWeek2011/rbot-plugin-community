b421482290425e475ad8315203e17dde4575c507
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-21 18:37:31 +0200

    add default admin user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create a default admin user
+User.create(:username => 'admin', :email => 'info@sixserv.org', :password => 'swordfish', :password_confirmation => 'swordfish')
+
