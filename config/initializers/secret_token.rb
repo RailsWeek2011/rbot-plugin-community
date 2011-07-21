6145a0d45b0244aad069109ae731d13968dce96f
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-21 11:19:45 +0200

    initial rails(3.1.0rc4) application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RbotPluginCommunity::Application.config.secret_token = 'ac3f190f28f6b0164246bf1926b0c8cef4769b17f4ed58380f83be7eecc3974f3871748f7a6e0524bc8143ad5807ccb2a36d2a634e2b890453725d30a0c272aa'
