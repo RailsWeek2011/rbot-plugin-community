e98784b3130bb1ed5835b69f2ad099d1606559ba
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-22 13:26:05 +0200

    allow anyone to see plugin index

--- app/controllers/plugins_controller.rb
+++ app/controllers/plugins_controller.rb
@@ -1,5 +1,5 @@
 class PluginsController < ApplicationController
-  before_filter :authenticate_user!, :except => [ :show ]
+  before_filter :authenticate_user!, :except => [ :index, :show ]
 
   # GET /plugins
   # GET /plugins.json
