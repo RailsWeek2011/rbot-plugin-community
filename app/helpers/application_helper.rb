007c9119cc21b7b97f018848762728ed833c611d
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-24 22:34:54 +0200

    first real design stylesheet and layout, unfinished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(user, size=16)
+    email_hash = Digest::MD5.hexdigest(user.email.strip.downcase)
+    "http://gravatar.com/avatar/#{email_hash}.png?s=#{size}&d=mm"
+  end
 end
