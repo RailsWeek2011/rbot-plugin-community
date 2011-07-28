8648bc7a0ac8b1dafc1b8ba962f30b343fc1d300
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-07-28 19:45:35 +0200

    use validation callbacks for rating model

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,27 +5,21 @@ class RatingsController < ApplicationController
 
     # create rating with user reference OR with ip entry:
     ip = request.remote_ip
+    # otherwise i get some very weird varchar binary blobs:
+    ip.encode! 'UTF-8' if ip.encoding.to_s == 'ASCII-8BIT'
     user = nil
     if user_signed_in?
       user = current_user
     end
 
-    if (user and not Rating.where('plugin_id = ? AND user_id = ?', @plugin.id, user.id).empty?) or 
-       (ip and not Rating.where('plugin_id = ? AND ip = ?', @plugin.id, ip).empty?)
-      respond_to do |format|
-        format.html { redirect_to @plugin, alert: 'Plugin already rated!' }
-        format.json { render json: @plugin, status: :not_rated }
-      end
-      return
-    end
     @rating = Rating.new(:plugin => @plugin, :user => user, :ip => ip, :rate => rate)
 
     respond_to do |format|
       if @rating.save
-        format.html { redirect_to @plugin, notice: 'Plugin rated.' }
+        format.html { redirect_to(plugin_url(@plugin), :flash => {notice: 'Plugin rated.'}) }
         format.json { render json: @plugin, status: :rated }
       else
-        format.html { redirect_to @plugin, alert: 'Plugin rating not saved!' }
+        format.html { redirect_to(plugin_url(@plugin), :flash => {alert: 'Plugin rating not saved!', errors: @rating.errors}) }
         format.json { render json: @plugin.errors, status: :unprocessable_entity }
       end
     end
